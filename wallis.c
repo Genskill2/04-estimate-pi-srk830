3bec1c0be4bb21900bdaf29615d930ee5ce5b26f
srk830 <84616986+srk830@users.noreply.github.com>
2021-06-01 13:32:38 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -3,17 +3,22 @@
 #include <stdio.h>
 #include <math.h>
 
+float wp[3001];
+
 float wallis_pi(int n) {
-  float rval = 2;
-  for (int i = 1; i <= n; i++) {
-    float p = 4*i*i;
-    rval *= p/(p-1);
-  }
-  return rval;
+  if (wp[n] >= 0) return wp[n];
+  float p = 4*n*n;
+  wp[n] = (p/(p-1))*wallis_pi(n-1);
+  return wp[n];
 }
 
 int main(void) {
   float pi;
+  wp[0] = 0;
+  wp[1] = 8.0/3;
+  for (int i = 2; i <= 3001; i++) {
+    wp[i] = -1;
+  }
   for (int i=0; i<5; i++) {
     pi = wallis_pi(i);
     if (!(fabs(pi - M_PI) > 0.15)) {
